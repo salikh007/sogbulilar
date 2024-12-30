094aa65c6614330293d6a0fdf18e89af36515c34
salikh007 <abdulhamidovmuhammadzayd@gmail.com>
2024-12-30 14:34:22 +0500

    izoh

--- dak/lib/main.dart
+++ dak/lib/main.dart
@@ -90,7 +90,7 @@ class _MyAppState extends State<MyApp> {
               },
               child: GestureDetector(
                   onTap: () {
-                    print("chiqdi");
+                    print("ch");
                     Navigator.push(
                         context,
                         MaterialPageRoute(
